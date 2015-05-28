e627aa134ee14ad823cb46283da1f1a8f6fc4420
FlorianMtin <florian.martin3@etu.univ-lyon1.fr>
2015-05-28 17:00:57 +0200

    image

--- WinGear/src/DAO/db/structure.sql
+++ WinGear/src/DAO/db/structure.sql
@@ -9,7 +9,8 @@ art_id integer not null primary key auto_increment,
 art_title varchar(100) not null,
 art_content varchar(2000) not null,
 art_image varchar(100) not null,
-art_price float not null
+art_price float not null,
+categorie int(11) NOT NULL
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 
 create table t_user (
@@ -17,7 +18,10 @@ usr_id integer not null primary key auto_increment,
 usr_name varchar(50) not null,
 usr_password varchar(88) not null,
 usr_salt varchar(23) not null,
-usr_role varchar(50) not null
+usr_role varchar(50) not null,
+usr_nom varchar(25)  NOT NULL,
+usr_prenom varchar(25)  NOT NULL,
+usr_email varchar(75)  NOT NULL,
 ) engine=innodb character set utf8 collate utf8_unicode_ci;
 
 create table t_comment (
